827740c9122dc50c04e1e77f4027450cdd2bdff3
ray-revenge <raysrevenge999@gmail.com>
2016-03-04 22:00:03 -0500

    Update builder.lua

--- builder.lua
+++ builder.lua
@@ -21,7 +21,9 @@
 local b4 = game.HttpService.HttpEnabled
 game.HttpService.HttpEnabled = true
 
-local dec do --found this function somewhere
+-- Lua 5.1+ base64 v3.0 (c) 2009 by Alex Kloss <alexthkloss@web.de>
+-- licensed under the terms of the LGPL2
+local dec do
 	local b="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 	
 	function dec(data)
